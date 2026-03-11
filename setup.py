0468f9f9e990557d34cedfd7f1a709d122ca2b4a
shreyashF130 <shreyashfulmali13@gmail.com>
2026-03-11 21:15:31 +0530

    Cookiecutter initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project will help to recommend top 3 food items on swiggy on home page',
+    author='Shreyash',
+    license='',
+)
